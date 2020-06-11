d87f206aefd2ea90d71efba8eee37ea611a1a0e9
Fernando Moraes Oliveira <Koynonia@users.noreply.github.com>
2020-06-11 20:06:16 -0300

    [Docker-Server] Criação do teste.sql para criação de uma Database e Tables para fins de testes.

--- docker-server/teste.sql
+++ docker-server/teste.sql
@@ -1,7 +1,10 @@
+IF DB_ID('teste') IS NULL
 CREATE DATABASE teste
+GO
 
 USE teste
 
+if OBJECT_ID('Funcionarios') IS NULL
 CREATE TABLE Funcionarios
 (
     Id Int identity(1,1),
@@ -12,7 +15,7 @@ CREATE TABLE Funcionarios
     Cidade Nvarchar(50) Not Null
 )
 
+if (SELECT COUNT(1) FROM Funcionarios) = 0
+INSERT INTO Funcionarios VALUES ('Fernando','M. Oliveira',5000,'M','São Paulo')
 
 SELECT * FROM Funcionarios
-
-INSERT INTO Funcionarios VALUES ('Fernando','M. Oliveira',5000,'M','São Paulo')
