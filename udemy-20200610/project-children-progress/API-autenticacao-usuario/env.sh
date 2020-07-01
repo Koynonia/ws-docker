5f76a4f9cfdd44e45923314e61eb660f15a27c2b
Fernando Moraes Oliveira <Koynonia@users.noreply.github.com>
2020-07-01 00:40:45 -0300

    Criação dos scripts de variáveis de ambiente

---
+++ udemy-20200610/project-children-progress/API-autenticacao-usuario/env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# variável de porta:
+PORT=3000 export PORT
+
+# variável do servidor
+MONGODB_URI="mongodb://localhost:27017/local" export MONGODB_URI
+
+# variável de debug:
+DEBUG="API-autenticacao-usuario*" export DEBUG
+
+# variável de chave de acesso (Apigee):
+SECRET="SmVzdXMgw6kgbyDDum5pY28gc2FsdmFkb3Ih" export SECRET
